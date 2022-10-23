7849a5bea8cc5ab24f123519993a4b9b5c78ff8e
Naeem-wq <95334435+Naeem-wq@users.noreply.github.com>
2022-10-23 23:59:26 +0200

    Final project

---
+++ db_conn.php
@@ -0,0 +1,25 @@
+<?php 
+
+# server name
+$sName = "localhost";
+# user name
+$uName = "naeemwitbooi4@gmail.com";
+# password
+$pass = "admin@123!";
+
+# database name
+$db_name = "online_book_store_db";
+
+/**
+creating database connection 
+useing The PHP Data Objects (PDO)
+**/
+try {
+    $conn = new PDO("mysql:host=$sName;dbname=$db_name", 
+                    $uName, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+  echo "Connection failed : ". $e->getMessage();
+}
+
+
